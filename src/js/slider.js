e7846d08eb01702b10421e96981b48f0c252ae9c
Tux <77551844+Pancho-Developer@users.noreply.github.com>
2022-01-28 13:19:35 -0300

    Navigation, Header and Slider

---
+++ src/js/slider.js
@@ -0,0 +1,20 @@
+ (() => {
+    const sliders = [...document.querySelectorAll('.slider__body')];
+    const arrowAfter = document.querySelector('#after');
+    const arrowBefore = document.querySelector('#before');
+    let value;
+
+    arrowAfter.addEventListener('click', () => changePosition(1));
+    arrowBefore.addEventListener('click', () => changePosition(-1));
+
+    function changePosition(change){
+        const currentElement = Number(document.querySelector('.slider__body--show').dataset.id);
+        value = currentElement;
+        value+=change;
+        if(value === 0 || value == sliders.length+1){
+            value =  value === 0 ? sliders.length : 1;
+        }
+        sliders[currentElement-1].classList.toggle('slider__body--show');
+        sliders[value-1].classList.toggle('slider__body--show');
+    }
+ })()
